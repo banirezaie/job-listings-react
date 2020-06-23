3190f253e39b5318a5320e6fd6921a20d1cd0b2f
BANI GIT <bani.rezaie@gmail.com>
2020-06-24 00:16:40 +0100

    header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from "react";
 import "./App.css";
+import header from "./bg-header-desktop.svg";
 
 function App() {
   return (
-    <div className="App-header">
-      <header>This is Header</header>
+    <div className="App">
+      <img src={header} className="App-logo" alt="header" />
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
